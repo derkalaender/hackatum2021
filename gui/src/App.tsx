0d9f4e3e84645bead284b082b7ccb9e7dbcd69af
derkalaender <git@derkalaender.de>
2021-11-20 19:54:55 +0100

    Add controls

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,13 +1,12 @@
 import {Box, Grid} from "@mui/material";
-import Controls from "./Controls";
-import React, {Suspense, useEffect, useState} from "react";
+import Controls, {TextFieldId} from "./Controls";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import {Status, Wrapper} from "@googlemaps/react-wrapper";
 import {Vehicle} from "./types";
 import {getBookings, getDirections, getVehicles} from "./api";
 import Polyline from "./map/Polyline";
 import Marker from "./map/Marker";
-
-const Map = React.lazy(() => import("./map/Map"))
+import Map from "./map/Map";
 
 const render = (status: Status) => {
     switch (status) {
@@ -21,8 +20,55 @@ const render = (status: Status) => {
 }
 
 const App = () => {
+    const latlngRegex = "\\d+(\\.\\d+)?,\\w?.\\d+(\\.\\d+)?"
+
     const [vehicles, setVehicles] = useState<Vehicle[]>([])
     const [bookedRoutes, setBookedRoutes] = useState<google.maps.LatLng[][]>([])
+
+    const [startLatLng, setStartLatLng] = useState<string>("")
+    const [destinationLatLng, setDestinationLatLng] = useState<string>("")
+    const [selectedTextField, setSelectedTextField] = useState<TextFieldId>(TextFieldId.START)
+
+    const correctLatLngFormat = (): boolean => {
+        return [startLatLng, destinationLatLng].every(s => s.match(latlngRegex))
+    }
+
+    const latLngToMarker = (latlng: string, label: string) => {
+        if(latlng.match(latlngRegex)) {
+            let splitted = latlng.split(",")
+            let converted = new google.maps.LatLng(parseFloat(splitted[0].trim()), parseFloat(splitted[1].trim()))
+            return <Marker position={converted} label={label}/>
+        }
+    }
+
+    const onStartValueChanged = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setStartLatLng(e.target.value)
+    }
+
+    const onDestinationValueChanged = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        setDestinationLatLng(e.target.value)
+    }
+
+    const onMapClick = (e: google.maps.MapMouseEvent) => {
+        let latlng = e.latLng!!
+        let coords = "" + latlng.lat() + ", " + latlng.lng()
+        switch (selectedTextField) {
+            case TextFieldId.START:
+                setStartLatLng(coords)
+                break
+            case TextFieldId.DESTINATION:
+                setDestinationLatLng(coords)
+                break
+        }
+    }
+
+    const onSearch = () => {
+
+    }
+
+    const onSubmit = () => {
+
+    }
 
     // get vehicles
     useEffect(() => {
@@ -50,29 +96,34 @@ const App = () => {
 
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Grid container direction="column" style={{padding: "40px"}}>
-                <Grid item>
-                    <h1>Robotaxi</h1>
+        <Grid container direction="column" style={{padding: "40px"}}>
+            <Grid item>
+                <h1>Robotaxi</h1>
+            </Grid>
+            <Grid container spacing={5} alignItems="center" flexGrow={1}>
+                <Grid item xs={8} style={{height: "100%"}}>
+                    <Wrapper apiKey="AIzaSyBXqG0fwm1zQ_XlPujzFV3OXbGkggCUBnA" render={render}
+                             libraries={["geometry"]}>
+                        <Map center={{lat: 48.155004, lng: 11.4717963}} zoom={11} onClick={onMapClick}>
+                            {vehicles.map(v => <Marker key={v.vehicleID}
+                                                       position={new google.maps.LatLng(v.lat, v.lng)}
+                                                       icon="taxi.png"/>)}
+                            {bookedRoutes.map(v => <Polyline path={v}/>)}
+
+                            {latLngToMarker(startLatLng, "A")}
+                            {latLngToMarker(destinationLatLng, "B")}
+                        </Map>
+                    </Wrapper>
                 </Grid>
-                <Grid container spacing={5} alignItems="center" flexGrow={1}>
-                    <Grid item xs={8} style={{height: "100%"}}>
-                        <Wrapper apiKey="AIzaSyBXqG0fwm1zQ_XlPujzFV3OXbGkggCUBnA" render={render}
-                                 libraries={["geometry"]}>
-                            <Map center={{lat: 48.155004, lng: 11.4717963}} zoom={11}>
-                                {vehicles.map(v => <Marker key={v.vehicleID}
-                                                           position={new google.maps.LatLng(v.lat, v.lng)}
-                                                           icon="taxi.png"/>)}
-                                {bookedRoutes.map(v => <Polyline path={v}/>)}
-                            </Map>
-                        </Wrapper>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <Controls/>
-                    </Grid>
+                <Grid item xs={4}>
+                    <Controls startValue={startLatLng}
+                              destinationValue={destinationLatLng} onStartValueChange={onStartValueChanged}
+                              onDestinationValueChange={onDestinationValueChanged} searchEnabled={correctLatLngFormat()}
+                              submitEnabled={correctLatLngFormat()} onSelectTextField={setSelectedTextField}
+                              selectedTextField={selectedTextField} onSearch={onSearch} onSubmit={onSubmit}/>
                 </Grid>
             </Grid>
-        </Suspense>
+        </Grid>
     );
 }
 
--- gui/src/Controls.tsx
+++ gui/src/Controls.tsx
@@ -1,18 +1,57 @@
-import {Button, Card} from "@mui/material";
-import {getVehicles} from "./api";
+import {Button, Card, Grid, TextField} from "@mui/material";
+import React, {ChangeEventHandler} from "react";
 
-const Map = () => {
-    const onButton = async () => {
-        let vehicles = await getVehicles();
+interface ControlsProps {
+    onSearch?: () => void
+    searchEnabled?: boolean
+    onSubmit?: () => void
+    submitEnabled?: boolean
+    onSelectTextField?: (id: TextFieldId) => void
+    selectedTextField?: TextFieldId
+    startValue?: string
+    onStartValueChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
+    destinationValue?: string
+    onDestinationValueChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
+}
 
-        console.log(vehicles);
-    }
+export enum TextFieldId {
+    START, DESTINATION
+}
 
+const Controls: React.FC<ControlsProps> = ({
+                                               onSearch,
+                                               searchEnabled,
+                                               onSubmit,
+                                               submitEnabled,
+                                               onSelectTextField,
+                                               selectedTextField,
+                                               startValue,
+                                               onStartValueChange,
+                                               destinationValue,
+                                               onDestinationValueChange
+                                           }) => {
     return (
-        <Card>
-            <Button onClick={onButton}>Click me</Button>
+        <Card sx={{p: 5}}>
+            <Grid container spacing={2} direction="column" alignItems="center" alignContent="center">
+                <Grid item>
+                    <TextField label="Start" onSelect={() => onSelectTextField?.(TextFieldId.START)} value={startValue}
+                               onChange={onStartValueChange} focused={selectedTextField === TextFieldId.START}/>
+                </Grid>
+                <Grid item>
+                    <TextField label="Destination" onSelect={() => onSelectTextField?.(TextFieldId.DESTINATION)}
+                               value={destinationValue}
+                               onChange={onDestinationValueChange}
+                               focused={selectedTextField === TextFieldId.DESTINATION}/>
+                </Grid>
+                <Grid item>
+                    <Button onClick={onSearch} disabled={searchEnabled === false}> Search</Button>
+                </Grid>
+                <Grid item>
+                    <Button onClick={onSubmit} disabled={submitEnabled === false}>Submit</Button>
+                </Grid>
+            </Grid>
         </Card>
-    );
-};
+    )
+}
 
-export default Map
+export default Controls
