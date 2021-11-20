208397d9c45252d9971987af099547daba8af89a
derkalaender <git@derkalaender.de>
2021-11-20 21:16:55 +0100

    Display stats

--- gui/src/Controls.tsx
+++ gui/src/Controls.tsx
@@ -1,4 +1,4 @@
-import {Button, Card, Grid, TextField} from "@mui/material";
+import {Button, Card, Chip, Grid, TextField, Tooltip} from "@mui/material";
 import React, {ChangeEventHandler} from "react";
 
 interface ControlsProps {
@@ -12,10 +12,20 @@ interface ControlsProps {
     onStartValueChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
     destinationValue?: string
     onDestinationValueChange?: ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>
+    displayValues?: DisplayValues
 }
 
 export enum TextFieldId {
     START, DESTINATION
+}
+
+export interface DisplayValues {
+    distanceStandard?: number
+    distanceEco?: number
+    timeStandard?: number
+    timeEco?: number
+    co2Standard?: number
+    co2Eco?: number
 }
 
 const Controls: React.FC<ControlsProps> = ({
@@ -28,8 +38,35 @@ const Controls: React.FC<ControlsProps> = ({
                                                startValue,
                                                onStartValueChange,
                                                destinationValue,
-                                               onDestinationValueChange
+                                               onDestinationValueChange,
+                                               displayValues
                                            }) => {
+
+    const displayDistance = (number: number | undefined): string => {
+        if(number === undefined) return "N/A"
+
+        return (number / 1000).toFixed(2) + "km"
+    }
+
+    const displayTime = (number: number | undefined): string => {
+        if(number === undefined) return "N/A"
+
+        return (number / 60 / 60).toFixed(2) + "h"
+    }
+
+    const calcCO2Saved = (): string => {
+        if (displayValues?.co2Standard === undefined || displayValues?.co2Eco === undefined) return "N/A"
+
+        return ((1 - displayValues.co2Eco / displayValues.co2Standard) * 100).toFixed(0) + "%"
+    }
+
+    const toolTipCO2 = (): string => {
+        if (displayValues?.co2Standard === undefined || displayValues?.co2Eco === undefined) return "N/A"
+
+        return "A normal trip would cost the environment a total of " + displayValues.co2Standard.toFixed(2) + "kg of CO2.\n" +
+            "But with carpooling it's only " + displayValues.co2Eco.toFixed(2) + "kg of CO2!"
+    }
+
     return (
         <Card sx={{p: 5}}>
             <Grid container spacing={2} direction="column" alignItems="center" alignContent="center">
@@ -49,6 +86,26 @@ const Controls: React.FC<ControlsProps> = ({
                 <Grid item>
                     <Button onClick={onSubmit} disabled={submitEnabled === false}>Submit</Button>
                 </Grid>
+
+                <Grid item>
+                    <Chip label={"Distance normal: " + displayDistance(displayValues?.distanceStandard)}/>
+                </Grid>
+                <Grid item>
+                    <Chip label={"Time normal: " + displayTime(displayValues?.timeStandard)}/>
+                </Grid>
+                <Grid item>
+                    <Chip label={"Distance Carpooling: " + displayDistance(displayValues?.distanceEco)}/>
+                </Grid>
+                <Grid item>
+                    <Chip label={"Time Carpooling: " + displayTime(displayValues?.timeEco)}/>
+                </Grid>
+                <Grid item>
+                    <Tooltip title={toolTipCO2()}>
+                        <Chip label={"CO2 saved: " + calcCO2Saved()} color="success"/>
+                    </Tooltip>
+
+                </Grid>
+
             </Grid>
         </Card>
     )
